d4c8f011f14b2e5cce7acd5356341c2d2cbe323e
EgoGreenal <2416994581@qq.com>
2023-06-04 13:16:54 +0800

    Update Basic Code Highlighting

--- utils.py
+++ utils.py
@@ -20,10 +20,11 @@ class Problem:
         self.name = name
 
 class Submision:
-    def __init__(self, runid=0, problem=0, user=None, result=None):
+    def __init__(self, runid=0, problem=0, exec_time=0, exec_mem=0, language=None, user=None, result=None):
         self.runid = runid
         self.problem = problem
+        self.exec_time = exec_time
+        self.exec_mem = exec_mem
+        self.language = language
         self.user = user
         self.result = result
-
-
